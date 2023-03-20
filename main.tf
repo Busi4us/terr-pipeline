0f7f1c0b401fc4296e83e5aba7d0ebd83c9bbb47
Busi4us <113306408+Busi4us@users.noreply.github.com>
2023-03-20 14:00:11 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,36 @@
+
+## Providers Block
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+     required_version = ">=1.1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+# CREATING VPC
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+   
+   tags = {
+    Name = "Terr-pipeline-vpc"   
+}
+}
+
+# USING COUNT TO CREATE PUBLIC SUBNETS
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.2.0/24"
+  
+  map_public_ip_on_launch = true #public ip
+
+  tags = {
+    Name = "terr-pipeline-subnet"   
+    }
+}
+
